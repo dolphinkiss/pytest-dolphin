3695b118f4a7e1ffe96db87e865888e2da33e145
Peter Lauri <peterlauri@gmail.com>
2016-09-08 14:36:39 +0200

    added patched browser fixture, that replaces pytest-splinter browser fixture

--- pytest_dolphin.py
+++ pytest_dolphin.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 
+import os
 import pytest
+import re
+
 from pytest_django.lazy_django import django_settings_is_configured
+from splinter.exceptions import ElementDoesNotExist
 
 
 def pytest_addoption(parser):
@@ -65,3 +69,108 @@ def _clear_django_site_cache():
         if 'django.contrib.sites' in dj_settings.INSTALLED_APPS:
             from django.contrib.sites import models
             models.SITE_CACHE.clear()
+
+
+@pytest.fixture(scope='session')
+def splinter_screenshot_dir():
+    return os.path.join('.debug')
+
+
+@pytest.fixture(scope='session')
+def splinter_webdriver():
+    """Override splinter webdriver name."""
+    return os.getenv('TEST_UI_DRIVER', 'phantomjs')
+
+
+def _browser_patcher(browser, live_server_reverse):
+    # b = Browser('phantomjs', service_log_path=settings.GHOST_DRIVER_LOG)
+    # b.driver.set_window_size(1024, 680)
+
+    # def logout():
+    #     browser.visit(live_server_url + reverse('base:logout'))
+    #     browser.cookies.delete()
+    #     browser.visit(live_server_url + reverse('base:login'))
+    #
+    # def login(email, password='password', logout_first=True):
+    #     if logout_first:
+    #         logout()
+    #     form = b.find_by_tag('form')
+    #     form.find_by_name('username').first.fill(email)
+    #     form.find_by_name('password').first.fill(password)
+    #     form.find_by_tag('button').first.click()
+
+    def click_path(*path):
+        for link in path:
+            item_offset = None
+
+            # if we have # in the end, we should use the Nth item
+            m = re.match(r'^(.+)#(\d+)$', link)
+            if m:
+                link = m.group(1)
+                item_offset = int(m.group(2)) - 1
+
+            wait_for_text_to_be_present = False
+            if link.startswith('wait:'):
+                link = link[5:]
+                wait_for_text_to_be_present = True
+
+            if link.startswith('id:'):
+                if wait_for_text_to_be_present:
+                    raise Exception('wait: and id: cannot be combined in click_path')
+                element_finder, text = browser.find_by_id, link[3:]
+            elif link.startswith('exact:'):
+                element_finder, text = browser.find_link_by_text, link[6:]
+            else:
+                element_finder, text = browser.find_link_by_partial_text, link
+
+            if wait_for_text_to_be_present:
+                browser.is_text_present(text)
+
+            elements = element_finder(text)
+            if item_offset is None:
+                elements.first.click()
+            else:
+                elements[item_offset].click()
+
+    def visit_with_reverse(path, hostname=None, *args, **kwargs):
+        browser.visit(live_server_reverse(path, hostname), *args, **kwargs)
+        assert browser.status_code.code != 500, 'Visting %s caused server error 500' % live_server_reverse(path)
+
+    def fill_and_submit_form(form_id, use_js_submit=False, **fields):
+        try:
+            form = browser.find_by_id(form_id).first
+        except ElementDoesNotExist as find_exec:
+            info_text = 'available forms: ' + ', '.join(
+                ['<id:%s, class:%s>' % (f['id'], f['class']) for f in browser.find_by_tag('form')]
+            )
+            raise ElementDoesNotExist('%s - %s' % (find_exec, info_text))
+
+        for k, v in fields.iteritems():
+            input_element = form.find_by_name(k).first
+            if isinstance(v, basestring) and v.startswith('select:'):
+                for select_value in v[7:].split(','):
+                    input_element.select(select_value)
+            elif isinstance(v, basestring) and v.startswith('check:'):
+                check, value = v.split(':')
+                if value.lower() == 'true':
+                    input_element.check()
+                else:
+                    input_element.uncheck()
+            else:
+                input_element.fill(v)
+
+        if use_js_submit:
+            browser.execute_script("document.getElementById('%s').submit();" % form_id)
+        else:
+            form.find_by_tag('button').first.click()
+
+    browser.django_visit = visit_with_reverse
+    browser.click_path = click_path
+    browser.fill_and_submit_form = fill_and_submit_form
+    return browser
+
+
+@pytest.fixture(scope='function')
+def browser(request, browser_instance_getter, live_server_reverse):
+    obj = browser_instance_getter(request, browser)
+    return _browser_patcher(obj, live_server_reverse)
