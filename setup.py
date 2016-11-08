57d288c22f8168a9422b2396d965d47a0596d57c
Peter Lauri <peterlauri@gmail.com>
2016-11-08 19:15:00 +0100

    Bump version: 0.3.2 → 0.3.3

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read(fname):
 
 setup(
     name='pytest-dolphin',
-    version='0.3.2',
+    version='0.3.3',
     author='Peter Lauri',
     author_email='peterlauri@gmail.com',
     maintainer='Peter Lauri',
